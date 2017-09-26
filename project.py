e6d16333eeed9aaafd7255e9b0cc2d0ed8126583
Peretz <peretzman@gmail.com>
2017-09-26 18:46:08 -0500

    Bug fix. The string values were not being replaced properly.y

--- project.py
+++ project.py
@@ -247,8 +247,7 @@ def fbdisconnect():
     # Execute HTTP request to revoke access token.
     facebook_id = login_session['facebook_id']
     access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?'
-    url += 'access_token=%s' % (facebook_id, access_token)
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id, access_token)
     h = httplib2.Http()
     resultStr = h.request(url, 'DELETE')[1]
     result = json.loads(resultStr)
